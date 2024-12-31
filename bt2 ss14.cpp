0f10808afbcb1ac2b0564453991b06a1f06dc2ee
bestamidu <ynhu22887@gmail.com>
2024-12-31 12:02:47 +0700

    2

---
+++ bt2 ss14.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+	char duci[]=" hom nay toi dep ";
+	int length = strlen(duci);
+	for ( int i =0 ;  i <length ;i++){
+		printf(" %c", duci[i]);
+
+	}
+	return 0;
+}
